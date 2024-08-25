17e2a660a7cfc106e24232ab219f6eda608f959c
Ad603-tech <adityabendale603@gmail.com>
2024-08-24 22:08:01 -0400

    project-4

---
+++ Udacity-Robotics-Robot_Mapping/roboslam_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pc-icms/roboslam_ws/src
+# Build directory: /home/pc-icms/roboslam_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
